6bf71dea652164f43feb2cfb4d06aecc906bfbde
Fernanda Queiroz <fernanda.cdqueiroz@gmail.com>
2019-06-23 20:27:36 -0300

    Toggles 'instalments' fields based on payment method

--- finerplan/static/js/form-visibility.js
+++ finerplan/static/js/form-visibility.js
@@ -10,11 +10,16 @@ var transactionKind
 
 // Changes form's fields based on transaction kind
 $(function() {
-  $( "#transactionRadios" ).change(function() {
+  $( "#transactionKind" ).change(function() {
     hideWobblyFields();
     transactionKind = $(this).find("input[name='transaction']:checked").val();
-    $( ".field-" + transactionKind ).css('display','block');
+    $( ".field-" + transactionKind ).css('display','flex');
+
+    // Clear some fields values
     $("#newCategoryCheck").prop('checked', false);
+    $("#paymentMethod").find("input[name='pay_method']:checked").prop('checked', false);
+    $("#paymentInstallments").css('display', 'none');
+    $("#paymentInstallments").find("input").val("1");
   });
 });
 
@@ -22,12 +27,24 @@ $(function() {
 $(function() {
   $( "#newCategoryCheck" ).change(function() {
     if($(this).is(":checked")) {
-      $(".field-new-category").css('display','block');
+      $(".field-new-category").css('display','flex');
       $(".field-category").filter(".field-" + transactionKind).css('display','none');
     } else {
       $(".field-new-category").css('display','none')
-      $(".field-category").filter(".field-" + transactionKind).css('display','block');
+      $(".field-category").filter(".field-" + transactionKind).css('display','flex');
       $("#new_cat").val("");
     }
+  });
+});
+
+// Toggles 'instalments' fields based on payment method
+$(function() {
+  $( "#paymentMethod" ).change(function() {
+    if( $(this).find("input[name='pay_method']:checked").val() == 'Crédito' ) {
+      $("#paymentInstallments").css('display', 'flex');
+    } else {
+      $("#paymentInstallments").css('display', 'none');
+    }
+    $("#paymentInstallments").find("input").val("1");  // Resets value
   });
 });
