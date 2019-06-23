1c5a4fa5e85b9c429053a203232cc63a0a7af090
Fernanda Queiroz <fernanda.cdqueiroz@gmail.com>
2019-06-23 19:58:42 -0300

    adds functionality to 'new category' checkbox, hiding and showing other fields accordingly

--- finerplan/static/js/form-visibility.js
+++ finerplan/static/js/form-visibility.js
@@ -6,9 +6,28 @@ function hideWobblyFields() {
   $(".field-wobbly").css('display','none')
 };
 
+var transactionKind
+
+// Changes form's fields based on transaction kind
 $(function() {
   $( "#transactionRadios" ).change(function() {
     hideWobblyFields();
-    $( ".field-" + $(this).find("input[name='transaction']:checked").val() ).css('display','block');
+    transactionKind = $(this).find("input[name='transaction']:checked").val();
+    $( ".field-" + transactionKind ).css('display','block');
+    $("#newCategoryCheck").prop('checked', false);
+  });
+});
+
+// Switches 'category' dropdown selector by 'new category' text field based on checkbox
+$(function() {
+  $( "#newCategoryCheck" ).change(function() {
+    if($(this).is(":checked")) {
+      $(".field-new-category").css('display','block');
+      $(".field-category").filter(".field-" + transactionKind).css('display','none');
+    } else {
+      $(".field-new-category").css('display','none')
+      $(".field-category").filter(".field-" + transactionKind).css('display','block');
+      $("#new_cat").val("");
+    }
   });
 });
