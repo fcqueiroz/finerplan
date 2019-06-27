467a4cf94dc98ce8a6782c39367ba016f8f26332
Fernanda Queiroz <fernanda.cdqueiroz@gmail.com>
2019-06-27 12:14:23 -0300

    includes flask-migrate in app factory

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,11 @@ def create_app(config_name):
     _app.config.from_object(app_config(config_name))
     with _app.app_context():
         db.init_app(_app)
+        migrate.init_app(_app, db)
 
     return _app
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 app = create_app(config_name='development')
-migrate = Migrate(app, db)
