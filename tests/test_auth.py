633ee75ddd66863170b2bebd8b627644d459a144
Fernanda Queiroz <fernanda.cdqueiroz@gmail.com>
2019-07-21 00:26:58 -0300

    splits auth tests into one more class

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,6 +9,8 @@ from tests.conftest import RoutingMixin, DatabaseMixin
 
 
 class BasicAuth(DatabaseMixin):
+    login_url = '/login'
+    register_url = '/register'
 
     def login(self, client, username=None, password=None, query_string=None):
         """Login helper function"""
@@ -17,7 +19,7 @@ class BasicAuth(DatabaseMixin):
         if password is None:
             password = self.test_user['password']
         
-        return client.post('/login', data=dict(
+        return client.post(self.login_url, data=dict(
             username=username,
             password=password
         ), follow_redirects=True, query_string=query_string)
@@ -36,7 +38,7 @@ class BasicAuth(DatabaseMixin):
         if email is None:
             email = self.test_user['email']
         
-        return client.post('/register', data=dict(
+        return client.post(self.register_url, data=dict(
             username=username,
             password=password,
             password2=password,
@@ -44,9 +46,35 @@ class BasicAuth(DatabaseMixin):
         ), follow_redirects=True)
 
 
-@pytest.mark.usefixtures('app_db_with_test_user')
-class TestUserAuth(RoutingMixin, BasicAuth):
+class TestAuthRoutes(RoutingMixin, BasicAuth):
+    def test_register_page_exists(self, client):
+        response = client.get(self.register_url)
+        self.check_200_status_code(response, self.register_url)
+        self.check_content_type(response, self.register_url)
 
+    def test_login_page_exists(self, client):
+        response = client.get(self.login_url)
+        self.check_200_status_code(response, self.login_url)
+        self.check_content_type(response, self.login_url)
+
+    @pytest.mark.parametrize("url", ['/overview', '/expenses'])
+    def test_url_is_inaccessible_before_login(self, client, url):
+        title = ('<title>' + url.replace('/', '').capitalize()).encode('utf-8')
+        with client:
+            response = client.get(url)
+            assert title not in response.data
+
+
+class TestUserRegister(BasicAuth):
+    def test_successful_register(self, client, app_db):
+        """Tests that a user can register in app"""
+        self.register(client)
+        user = app_db.session.query(User).filter_by(username=self.test_user['username']).first()
+        assert user is not None
+
+
+@pytest.mark.usefixtures('app_db_with_test_user')
+class TestUserLogin(BasicAuth):
     def test_successful_login(self, client):
         with client:
             self.login(client)
@@ -67,38 +95,10 @@ class TestUserAuth(RoutingMixin, BasicAuth):
         rv = self.login(client, password=self.test_user['password'] + 'x')
         assert b'Invalid password' in rv.data
 
-    def test_login_page_exists(self, client):
-        url = '/login'
-        response = client.get(url)
-        self.check_200_status_code(response, url)
-        self.check_content_type(response, url)
-
-    @pytest.mark.parametrize("url", ['/overview', '/expenses', '/assets'])
-    def test_url_is_inaccessible_before_login(self, client, url):
-        title = ('<title>' + url.replace('/', '').capitalize()).encode('ascii')
-        with client:
-            response = client.get(url)
-            assert title not in response.data
-
-    @pytest.mark.parametrize("url", ['/overview', '/expenses', '/assets'])
+    @pytest.mark.parametrize("url", ['/overview', '/expenses'])
     def test_login_redirects_to_private_url(self, client, url):
-        title = ('<title>' + url.replace('/', '').capitalize()).encode('ascii')
+        title = ('<title>' + url.replace('/', '').capitalize()).encode('utf-8')
         with client:
             query_string = {'next':  url}
             response = self.login(client, query_string=query_string)
             assert title in response.data
-
-
-class TestUserRegister(RoutingMixin, BasicAuth):
-    def test_register_page_exists(self, client):
-        url = '/register'
-        response = client.get(url)
-        self.check_200_status_code(response, url)
-        self.check_content_type(response, url)
-
-    def test_successful_register(self, client, app_db):
-        """Tests that a user can register in app"""
-        self.register(client)
-
-        user = app_db.session.query(User).filter_by(username=self.test_user['username']).first()
-        assert user is not None
