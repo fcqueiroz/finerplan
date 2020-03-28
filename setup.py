4423c7ab58a2afbf8c43d6550b5b9d899b6c732d
Fernanda Queiroz <fernanda.cdqueiroz@gmail.com>
2020-03-28 15:34:54 -0300

    update version and package requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 setup(
     name='finerplan',
-    version='0.3',
+    version='0.3.1',
     packages=['finerplan'],
     include_package_data=True,
     install_requires=[
         'flask',
+        'python-dotenv',
+        'Flask-WTF',
+        'pandas',
     ],
     python_requires='>=3',
     setup_requires=[
