2e8db0b6097b023894328429b8cb540246f6b56f
Fernanda C. Queiroz <fernanda.cdqueiroz@gmail.com>
2020-04-12 18:40:46 -0300

    update setup.py to read requirements.txt

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'requirements.txt')) as f:
+    install_requires = f.read().splitlines()
 
 setup(
     name='finerplan',
     version='0.3.2',
     packages=['finerplan'],
     include_package_data=True,
-    install_requires=[
-        'flask',
-        'python-dotenv',
-        'Flask-WTF',
-        'pandas',
-    ],
+    install_requires=install_requires,
     python_requires='>=3.5',
 )
