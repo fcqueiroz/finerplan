d20a02e48512b89d17ffaf4893e70855becc8f80
Fernanda Queiroz <fernanda.cdqueiroz@gmail.com>
2020-05-08 00:19:01 -0300

    increment docs

--- finerplan/database.py
+++ finerplan/database.py
@@ -4,7 +4,7 @@ from flask import g, current_app, Flask
 
 
 class SqliteDatabase(object):
-    """Control a Sqlite Database bounded to a Flask application context."""
+    """Sqlite Database bounded to a Flask application context."""
     def __init__(self, flaskapp: Flask = None):
         self._app = flaskapp
         if flaskapp is not None:
@@ -12,24 +12,19 @@ class SqliteDatabase(object):
 
     def init_app(self, flaskapp: Flask):
         """Initializes the flask application."""
-        flaskapp.teardown_appcontext(self.teardown_db)
+        flaskapp.teardown_appcontext(self._teardown)
 
     @staticmethod
-    def connect():
-        """Open a connection to the database storage."""
+    def connect() -> sqlite3.Connection:
+        """Create or reuse an open connection to the database."""
         if 'db' not in g:
             database = current_app.config['SQLITE_DATABASE']
             g.db = sqlite3.connect(database)
         return g.db
 
     @staticmethod
-    def teardown_db(*args):
-        """Close the database if it exists.
-
-        Register this function as a Flask instance teardown_appcontext()
-        handler to guarantee its execution, even if a before-request
-        handler failed or was never executed.
-        """
+    def _teardown(*args):
+        """Close the database if it exists."""
         _db = g.pop('db', None)
         if _db is not None:
             _db.close()
